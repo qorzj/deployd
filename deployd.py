d5322d30bd7bc4c0ce7fe8fb84a66dbf3a81f1f6
zhangji <goodhorsezxj@gmail.com>
2017-05-07 10:46:37 +0800

    支持.deployd.conf

--- deployd.py
+++ deployd.py
@@ -1,24 +1,49 @@
 import daemon
 import web
 import os
+import random
 
 app = web.application()
 
 class FileReceiver:
-    def POST(self):
-        web_input = web.input(data={})
+    def POST(self, path):
+        web_input = web.input(data={}, cmd='')
         f = web_input.data
-        os.chdir('/mnt/grus_product')
-        open('bin/' + f.filename, 'wb').write(f.value)
-        os.system('docker-compose stop')
-        #os.system('docker-compose up -d')
-        return 'done.'
+        cmd = web_input.cmd
+
+        if not f:
+            return 'data not given'
+        if not cmd:
+            return 'cmd not given'
+
+        conf_name = os.path.join(path, '.deployd.conf')
+        if not os.access(conf_name, os.F_OK):
+            return '.deployd.conf not found'
+
+        try:
+            http_token = web.ctx.env.get('HTTP_TOKEN')
+            for line in open(conf_name):
+                if not line.strip() or line.startswith('#'): continue
+                ip, token = line.split()
+                if ip == web.ctx.ip and token == http_token:
+                    break
+            else:
+                return 'access denied'
+        except Exception as e:
+            return 'invalid .deployd.conf: ' + str(e)
+
+        os.chdir(path)
+        open(f.filename, 'wb').write(f.value)
+        os.system(cmd)
+        return 'ok'
 
     def GET(self):
-        return 'working'
+        chars = 'abcdefghijklmnopqrstuvwxyz'
+        chars += chars.upper() + '0123456789'
+        return ''.join([random.choice(chars) for i in range(64)])
 
 
-app.add_mapping('/', FileReceiver)
+app.add_mapping('(.+)', FileReceiver)
 
 
 with daemon.DaemonContext():
