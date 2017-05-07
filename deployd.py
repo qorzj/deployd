b5a9571c1612fd1f74665ce6ffb09c330257bb71
zhangji <goodhorsezxj@gmail.com>
2017-05-07 11:04:09 +0800

    fix bug

--- deployd.py
+++ deployd.py
@@ -11,7 +11,7 @@ class FileReceiver:
         f = web_input.data
         cmd = web_input.cmd
 
-        if not f:
+        if len(f) == 0:
             return 'data not given'
         if not cmd:
             return 'cmd not given'
